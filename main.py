204821a3b6c01e3436e262e2fd79d03a266fcd42
Hena ATCX Uholnikov <atcx@ukr.net>
2022-06-21 11:53:32 +0200

    Telegram Bot with currency exchange rate checker

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
-# This is a sample Python script.
+import config
+import telebot
+import requests
+from telebot import types
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+bot = telebot.TeleBot(config.token)
 
+# Decoding json, function json()
+response = requests.get(config.url).json()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@bot.message_handler(commands=["start", "help"])
+def send_welcome(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    itembtn1 = types.KeyboardButton("USD")
+    itembtn2 = types.KeyboardButton("EUR")
+    markup.add(itembtn1, itembtn2)
+    msg = bot.send_message(message.chat.id,
+                           "Привіт! \nНаразі цей Бот знаходиться на етапі розробки. \nАле поки ти можеш дізнатися курси валют", reply_markup=markup)
+    bot.register_next_step_handler(msg, process_coin_step)
 
+def process_coin_step(message):
+    try:
+        markup = types.ReplyKeyboardRemove(selective=False)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+        for coin in response:
+            if (message.text == coin["ccy"]):
+                bot.send_message(message.chat.id, printCoin(coin["buy"], coin["sale"]),
+                                 reply_markup=markup, parse_mode="Markdown")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    except Exception as e:
+        bot.reply_to(message, "ooops!")
+
+def printCoin(buy, sale):
+    """Виведення курсу користувачу"""
+    return " *Курс купівлі:* " + str(buy) + "\n  *Курс продажу:* " + str(sale)
+
+bot.enable_save_next_step_handlers(delay=2)
+
+bot.load_next_step_handlers()
+
+if __name__ == "__main__":
+    bot.polling(none_stop=True)
